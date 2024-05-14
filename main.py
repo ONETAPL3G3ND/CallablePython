336cdecdfa057fda53764c3b0aa8a2f38b928e45
Sergii Rudniev <100538048+ONETAPL3G3ND@users.noreply.github.com>
2024-05-14 22:07:16 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+def test():
+    print("Calling")
+
+
+class Test:
+    def __init__(self):
+        self.test = "example str"
+
+    def Test(self):
+        print("test")
+
+
+if __name__ == "__main__":
+    t = Test()
+
+    print(callable(test))
+    for i in vars(t):
+        if callable(vars(t).get(i)):
+            print(vars(t).get(i))
+        else:
+            print(vars(t).get(i))
+
